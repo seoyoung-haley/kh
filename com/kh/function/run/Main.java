9f605da2637eb4ea5e6f1a6a204b0a2eedf1a83b
Haley_b <63996851+seoyoung-haley@users.noreply.github.com>
2022-01-26 01:09:50 +0900

    Add files via upload

---
+++ com/kh/function/run/Main.java
@@ -0,0 +1,20 @@
+package com.kh.function.run;
+
+import java.util.Scanner;
+
+import com.kh.function.Example;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Example exm = new Example();
+		exm.opSample1();
+		exm.opSample2();
+		exm.opSample3();
+		exm.opSample4();
+	
+		Test x = new Test();
+		x.test();
+	}
+}
---
+++ com/kh/function/run/Test.java
@@ -0,0 +1,18 @@
+package com.kh.function.run;
+
+public class Test {
+
+	
+	
+	public static void test() {
+				
+		int a = 55;
+		int b = a + 10; 
+		double c = ((double) a++ / 3.0); 
+		
+		System.out.printf("%.2f%n", c); 
+		System.out.println(a); 
+				
+
+	}
+}
